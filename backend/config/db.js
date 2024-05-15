9e822c4fbebc1c4dc1c36fe7eeb8b8b35d406f02
TrailblaZer132 <uditjeet@gmail.com>
2024-05-15 19:31:20 +0530

    chore: Updated the product backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Successfully connected to MongoDB ✅`);
+    console.log(`Successfully connected to MongoDB 🍃`);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error 🍂: ${error.message}`);
     process.exit(1);
   }
 };
-
 
 export default connectDB;
