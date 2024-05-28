a0b83b00f4c1d0e33e2c2b0e0d2283961c00e44c
TrailblaZer132 <uditjeet@gmail.com>
2024-05-28 14:36:54 +0530

    Price change

--- frontend/src/pages/Products/ProductCarousel.jsx
+++ frontend/src/pages/Products/ProductCarousel.jsx
@@ -62,7 +62,7 @@ const ProductCarousel = () => {
                 <div className="mt-4 flex justify-between">
                   <div className="one">
                     <h2>{name}</h2>
-                    <p> $ {price}</p> <br /> <br />
+                    <p> ₹ {price}</p> <br /> <br />
                     <p className="w-[25rem]">
                       {description.substring(0, 170)} ...
                     </p>
